3490f2617c4863e9e3bad2f17fa81cbc7d5c0bd5
Yehor Radchenko <y.p.radchenko@gmail.com>
2024-04-12 10:11:43 +0300

    Lab2

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,25 @@
-const { drizzle } = require('drizzle-orm/node-postgres');
-const { Client } = require('pg');
-const schema = require('./schema');
+
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+import { Client } from 'pg';
+
+import * as schema from './schema';
+
 
 const client = new Client({
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'nodejs_course_admin',
-    password: 'my_password',
-    database: 'nodejs_course_database',
+
+   host: '127.0.0.1',
+
+   port: 5432,
+
+   user: 'nodejs_course_admin',
+
+   password: 'my_password',
+
+   database: 'nodejs_course_database',
+
 });
 
-client.connect();
+await client.connect();
 
-module.exports = drizzle(client, { schema });
+export const db = drizzle(client, { schema });
